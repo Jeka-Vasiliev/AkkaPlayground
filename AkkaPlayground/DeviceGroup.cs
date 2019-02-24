6af21344baf3aa2b57db654601308123d44d63ab
Jeka_V <zheka_v@inbox.ru>
2019-02-24 16:01:55 +0300

    device group

--- AkkaPlayground/DeviceGroup.cs
+++ AkkaPlayground/DeviceGroup.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Akka.Actor;
+using Akka.Event;
+using AkkaPlayground.MainDevice;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -22,7 +25,45 @@ namespace AkkaPlayground
         private DeviceRegistered() { }
     }
 
-    class DeviceGroup
+    public class DeviceGroup : UntypedActor
     {
+        private Dictionary<string, IActorRef> deviceIdToActor = new Dictionary<string, IActorRef>();
+
+        public DeviceGroup(string groupId)
+        {
+            GroupId = groupId;
+        }
+
+        public ILoggingAdapter Log { get; } = Context.GetLogger();
+        public string GroupId { get; }
+
+        protected override void PreStart() => Log.Info($"Device group {GroupId} started");
+        protected override void PostStop() => Log.Info($"Device group {GroupId} stopped");
+
+
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case RequestTrackDevice trackMsg when trackMsg.GroupId.Equals(GroupId):
+                    if (deviceIdToActor.TryGetValue(trackMsg.DeviceId, out var actorRef))
+                    {
+                        actorRef.Forward(trackMsg);
+                    }
+                    else
+                    {
+                        Log.Info($"Creating device actor for {trackMsg.DeviceId}");
+                        var deviceActor = Context.ActorOf(Device.Props(trackMsg.GroupId, trackMsg.DeviceId), $"device-{trackMsg.DeviceId}");
+                        deviceIdToActor.Add(trackMsg.DeviceId, deviceActor);
+                        deviceActor.Forward(trackMsg);
+                    }
+                    break;
+                case RequestTrackDevice trackMsg:
+                    Log.Warning($"Ignoring TrackDevice request for {trackMsg.GroupId}. This actor is responsible for {GroupId}.");
+                    break;
+            }
+        }
+
+        public Props Props(string groupId) => Akka.Actor.Props.Create(() => new DeviceGroup(groupId));
     }
 }
